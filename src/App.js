f6cc624d1eebf21ce745930347c9e8d7591be189
Adi Zimmerman <adizim@berkeley.edu>
2017-04-11 13:40:53 -0700

    added list and mapping functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 require('./App.css');
 
+const list = [
+	{
+		title: 'React',
+		url: 'https://facebook.github.io/react/',
+		author: 'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: 'Redux',
+		url: 'https://github.com/reactjs/redux',
+		author: 'Dan Abramov, Andrew Clark',
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
 class App extends Component {
   render() {
-    const helloWorld = 'yo to the Road to learn React';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+      	{list.map(function(item) {
+      		return (
+      			<div key={item.objectID}>
+      				<span>
+      					<a href={item.url}>{item.title}</a>
+      				</span>
+      				<span> Author: {item.author}</span>
+              		<span> Comments: {item.num_comments}</span>
+             		<span> Points: {item.points}</span>
+      			</div>
+      		);
+      	})}
       </div>
     );
   }
