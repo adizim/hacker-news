feabce90a127ec7d68d73da8b1edd0cb017b4797
Adi Zimmerman <adizim@berkeley.edu>
2017-04-12 15:29:49 -0700

    fetching from HN and cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 require('./index.css');
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Adi',
-  lastName: 'Zimmerman'
-};
-
-const element = (
-	<h1>
-		Hello, {formatName(user)}!
-	</h1>
-);
-
 ReactDOM.render(
   <App />,
   document.getElementById('content')
