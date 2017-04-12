10d2fd5398a204c350cddc2125480acc830d8c7b
Adi Zimmerman <adizim@berkeley.edu>
2017-04-12 09:59:04 -0700

    split up App into Search and Table components

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const list = [
 
 const isSearched = searchTerm => item => 
 	!searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
+
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -45,35 +46,64 @@ class App extends Component {
 	}
 
   render() {
+  	const { searchTerm, list } = this.state;
     return (
       <div className="App">
-      	<form>
-      		<input 
-	      		type="form"
-	      		onChange={this.onSearchChange}
-      		/>
-      	</form>
-      	{this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-  			<div key={item.objectID}>
-  				<span>
-  					<a href={item.url}>{item.title}</a>
-  				</span>
-  				<span> Author: {item.author}</span>
-          		<span> Comments: {item.num_comments}</span>
-         		<span> Points: {item.points}</span>
-         		<span>
-         			<button
-         				onClick={() => this.onDismiss(item.objectID)}
-         				type="button"
-         			>
-         			Dismiss
-         			</button>
-         		</span>
-  			</div>	
-      	)}
+      	<Search 
+      		value={searchTerm}
+      		onChange={this.onSearchChange}
+      	/>
+      	<Table
+      		list={list}
+      		pattern={searchTerm}
+      		onDismiss={this.onDismiss} 
+      	/>
       </div>
     );
   }
+}
+
+class Search extends Component {
+	render() {
+		const { value, onChange } = this.props;
+		return (
+			<form>
+	      		<input 
+		      		type="form"
+		      		value={value}
+		      		onChange={onChange}
+	      		/>
+      		</form>
+		);
+	}
+}
+
+class Table extends Component {
+	render() {
+		const { list, pattern, onDismiss } = this.props;
+		return (
+			<div>
+			{list.filter(isSearched(pattern)).map(item =>
+	  			<div key={item.objectID}>
+	  				<span>
+	  					<a href={item.url}>{item.title}</a>
+	  				</span>
+	  				<span> Author: {item.author}</span>
+	          		<span> Comments: {item.num_comments}</span>
+	         		<span> Points: {item.points}</span>
+	         		<span>
+	         			<button
+	         				onClick={() => onDismiss(item.objectID)}
+	         				type="button"
+	         			>
+	         			Dismiss
+	         			</button>
+	         		</span>
+	  			</div>	
+      		)}
+      		</div>
+		);
+	}
 }
 
 export default App;
